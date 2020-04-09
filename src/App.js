8e54d478922db5e1272ca88dd5915d79577d99c9
Jun-cloud <kjy8827@gmail.com>
2020-04-09 21:07:27 +0900

    How to use hook in function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -13,10 +13,21 @@ function App() {
 
 // 여기서 props는 변수의 이름일뿐 다른 이름으로 변경해도 상관없다.
 function FuncComp(props) {
+  // useState : function style에서 state를 만들때 사용; 매개변수는 state의 초기값; 배열을 리턴
+  var numberState = useState(props.initNumber);
+  // 배열의 첫번째는 상태값
+  var number = numberState[0];
+  // 배열의 두번째는 상태를 변경할 수 있는 함수
+  var setNumber = numberState[1];
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <input type="button" value="random" onClick={
+          function() {
+            setNumber(Math.random());
+          }
+        }></input>
     </div>
   );
 }
@@ -33,7 +44,8 @@ class ClassComp extends React.Component{
         <input type="button" value="random" onClick={
           function() {
             this.setState({number:Math.random()})
-        }.bind(this)}></input>
+          }.bind(this)
+        }></input>
       </div>
     );
   }
